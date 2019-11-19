2ca03489bdb929fa81e47a746875dfc445b87a74
Christian Zelaya - PC-01 <christian@cerouno.com.py>
2019-11-19 14:33:58 -0300

    Feature PROCESAR PARAMETROS

--- src/routes.php
+++ src/routes.php
@@ -1,3 +1,4 @@
 <?php
 	require __DIR__.'/../app/get.php';			//SOLICITUD GET
-	require __DIR__.'/../app/post.php';			//SOLICITUD GET
+	require __DIR__.'/../app/post.php';			//SOLICITUD POST
+	require __DIR__.'/../app/put.php';			//SOLICITUD PUT
