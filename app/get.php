db334643c0adea904c36c03361ec5fac9f6e774a
Christian Zelaya <christian@cerouno.com.py>
2019-12-17 11:14:27 -0300

    Bugs DOCUMENTO

--- app/get.php
+++ app/get.php
@@ -2523,7 +2523,7 @@
                 FROM [CSF_PRUEBA].[dbo].[empleados_AxisONE] a
                 INNER JOIN [CSF_PRUEBA].[dbo].[empleados_AxisONE] b ON a.CodCargoSuperior = b.CodigoCargo
 
-                WHERE b.CedulaEmpleado = ?";
+                WHERE a.CedulaEmpleado = ? OR b.CedulaEmpleado = ?";
             
             $sql01  = "SELECT
                 a.SOLFICCOD         AS          solicitud_codigo,
@@ -2567,7 +2567,7 @@
                 FROM [CSF_PERMISOS].[adm].[SOLFIC] a
                 INNER JOIN [CSF_PERMISOS].[adm].[DOMPAR] b ON a.SOLFICTST = b.DOMPARCOD
 
-                WHERE a.SOLFICDOC = ? OR a.SOLFICDOC = ?";
+                WHERE a.SOLFICDOC = ?";
 
             try {
                 $connMSSQL  = getConnectionMSSQL();
@@ -2578,7 +2578,7 @@
                 $stmtMSSQL01= $connMSSQL->prepare($sql01);
 
                 while ($rowMSSQL00 = $stmtMSSQL00->fetch()) {
-                    $stmtMSSQL01->execute([$val01, $rowMSSQL00['documento']]);
+                    $stmtMSSQL01->execute([$rowMSSQL00['documento']]);
 
                     while ($rowMSSQL01 = $stmtMSSQL01->fetch()) {
                         switch ($rowMSSQL01['solicitud_estado_codigo']) {
